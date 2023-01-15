ca35fc4e5dd2d2d904c450a8e7f7f747135d23ac
= <=>
2023-01-16 01:45:52 +0700

    feat: finished the landing page

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,3 +70,230 @@ translatorBtnClose.addEventListener("click", () => {
   firstNav.classList.remove("slip");
   translatorSlip.classList.remove("slip__up");
 });
+
+const listNameClub = [
+  "DON'T MISS",
+  "Home",
+  "Schedule",
+  "First Team Results",
+  "First Team Schedule",
+];
+
+const listFirstTeams = [
+  "FIRST TEAM",
+  "Latest",
+  "Schedule",
+  "Tickets",
+  "Results",
+  "Standings",
+  "Players",
+  "Photos",
+  "Honours",
+  "Features",
+];
+
+const listTicketsandMuseum = [
+  "TICKETS & MUSEUM",
+  "Men's Football",
+  "VIP Men's Football",
+  "Tour & museum",
+  "Basketball",
+  "VIP Basketball",
+  "Women's Football",
+  "Barcaland",
+  "Barca Atletic",
+  "Handball",
+  "Fulsal",
+  "Roller-Hockey",
+  "Ice Rink",
+  "Packs and Promotions",
+  "All about tickets",
+  "Barca Business",
+  "Barca Cafe",
+];
+
+const listBarcaTVs = [
+  "BARÇA TV+",
+  "Home",
+  "Live TV",
+  "My List",
+  "Barca & the World Cup",
+  "Subscriptio & Prices",
+  "Players",
+];
+
+const listClubs = [
+  "CLUB",
+  "Espai Barca",
+  "Lastest",
+  "Culers",
+  "Kids",
+  "Schedule",
+  "Members",
+  "More than a Club",
+  "Identity",
+  "Partners",
+  "History",
+  "Barca Foundation",
+  "Barca Academy",
+];
+
+const listBarcaTeams = [
+  "BARCA TEAMS",
+  "Football",
+  "Basketball",
+  "Handball",
+  "Futsal",
+  "Roller Hockey",
+  "Esports",
+];
+
+const listSubFootball = [
+  "First Team",
+  "Women's",
+  "Barça Atlètic",
+  "FC Barcelona U19A",
+  "Barça Legends",
+  "Barça Youth",
+  "Barça Genuine F.",
+  "Summer Camp",
+];
+
+const listSubBasketball = ["First Team"];
+
+const listSubHandball = ["First Team"];
+
+const listSubBFutsal = ["First Team"];
+
+const listSubRollerHockey = ["First Team"];
+
+const listSuEsports = ["eFootball PES", "League of Legends"];
+
+const listMiddleContent = document.getElementById("renderData");
+const listMiddleContents = document.querySelectorAll(
+  ".middle__first__navigation-item"
+);
+
+function renderData(array) {
+  listMiddleContent.innerHTML = "";
+  for (let i = 0; i < array.length; i++) {
+    if (i == 0) {
+      listMiddleContent.innerHTML += `
+      <li class="middle__second__navigation-item title">
+        <a href="#!">${array[i]}</a>
+      </li>
+    `;
+    } else {
+      listMiddleContent.innerHTML += `
+      <li class="middle__second__navigation-item">
+        <a href="#!">${array[i]}</a>
+      </li>
+    `;
+    }
+  }
+}
+
+function renderDataPlus(array) {
+  listMiddleContent.innerHTML = "";
+  for (let i = 0; i < array.length; i++) {
+    if (i == 0) {
+      listMiddleContent.innerHTML += `
+      <li class="middle__second__navigation-item title">
+        <a href="#!"><span>${array[i]}</span></a>
+      </li>
+    `;
+    } else {
+      listMiddleContent.innerHTML += `
+      <li class="middle__second__navigation-item" id="${array[i]}" onclick="btnToggle(this)">
+        <a href="#!">
+          <span>${array[i]}</span>
+          <i class="fa-solid fa-plus"></i>
+        </a>
+        <ul class="middle__second__navigation-subList"></ul>
+      </li>
+    `;
+    }
+  }
+}
+
+function renderSubData(array, content) {
+  content.innerHTML = "";
+  for (let i = 0; i < array.length; i++) {
+    content.innerHTML += `
+      <li class="middle__second__navigation-subItem">
+        <a href="#!">${array[i]}</a>
+      </li>
+    `;
+  }
+}
+
+function activeElement(elements) {
+  elements.forEach((e) => {
+    e.classList.remove("active");
+  });
+}
+
+function HandlerClick(event) {
+  if (event.id === "firstteams") {
+    renderData(listFirstTeams);
+    activeElement(listMiddleContents);
+    event.classList.add("active");
+    checkClassList(listMiddleContent);
+  } else if (event.id === "tickets") {
+    renderData(listTicketsandMuseum);
+    activeElement(listMiddleContents);
+    event.classList.add("active");
+    checkClassList(listMiddleContent);
+  } else if (event.id === "barcaTV") {
+    renderData(listBarcaTVs);
+    activeElement(listMiddleContents);
+    event.classList.add("active");
+    checkClassList(listMiddleContent);
+  } else if (event.id === "club") {
+    renderData(listClubs);
+    activeElement(listMiddleContents);
+    event.classList.add("active");
+    checkClassList(listMiddleContent);
+  } else if (event.id === "barcaTeams") {
+    renderDataPlus(listBarcaTeams);
+    activeElement(listMiddleContents);
+    event.classList.add("active");
+    checkClassList(listMiddleContent);
+  } else if (event.id === "nameclub") {
+    renderData(listNameClub);
+    activeElement(listMiddleContents);
+    event.classList.add("active");
+    checkClassList(listMiddleContent);
+  }
+}
+
+function checkClassList(node) {
+  node.classList.remove("slip__left");
+  setTimeout(() => {
+    node.classList.add("slip__left");
+  }, 300);
+}
+
+function btnToggle(e) {
+  e.children[1].classList.toggle("d_block");
+  e.children[0].children[1].classList.toggle("fa-plus");
+  e.children[0].children[1].classList.toggle("fa-minus");
+  if (e.id === "Football") {
+    renderSubData(listSubFootball, e.children[1]);
+  }
+  if (e.id === "Basketball") {
+    renderSubData(listSubBasketball, e.children[1]);
+  }
+  if (e.id === "Handball") {
+    renderSubData(listSubHandball, e.children[1]);
+  }
+  if (e.id === "Futsal") {
+    renderSubData(listSubBFutsal, e.children[1]);
+  }
+  if (e.id === "Roller Hockey") {
+    renderSubData(listSubRollerHockey, e.children[1]);
+  }
+  if (e.id === "Esports") {
+    renderSubData(listSuEsports, e.children[1]);
+  }
+}
